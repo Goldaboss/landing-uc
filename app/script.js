1e6d25b89590e08d8469e57086bb935f93027845
Valeriy <goldaboss@yandex.ru>
2019-12-08 15:27:10 +0300

    tabs

--- app/script.js
+++ app/script.js
@@ -1,9 +1,18 @@
-$(document).ready(function(){
+$(document).ready(function () {
     $(".js-anchor-link").on("click", function (evt) {
         evt.preventDefault();
-        const id  = $(this).attr("href"),
-              top = $(id).offset().top;
+        const id = $(this).attr("href"),
+            top = $(id).offset().top;
         $("body,html").animate({scrollTop: top}, 500);
-    })
+    });
+
+    $(".js-tab-button").on("click", function () {
+        const tabClass = $(this).attr("data-tab-class"),
+            block = $('.' + tabClass);
+        $(".js-tab-button").removeClass("fourth__main-header-btn-active");
+        $(this).addClass("fourth__main-header-btn-active");
+        $(".fourth__main-scheme").addClass("fourth__main-scheme-none");
+        block.removeClass("fourth__main-scheme-none");
+    });
 });
 
--- docs/js/script.js
+++ docs/js/script.js
@@ -9,4 +9,12 @@ $(document).ready(function () {
       scrollTop: top
     }, 500);
   });
+  $(".js-tab-button").on("click", function () {
+    var tabClass = $(this).attr("data-tab-class"),
+        block = $('.' + tabClass);
+    $(".js-tab-button").removeClass("fourth__main-header-btn-active");
+    $(this).addClass("fourth__main-header-btn-active");
+    $(".fourth__main-scheme").addClass("fourth__main-scheme-none");
+    block.removeClass("fourth__main-scheme-none");
+  });
 });
