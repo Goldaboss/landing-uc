6144fd3b663869205020cfb027d99db4e8fe7ddd
Valeriy <goldaboss@yandex.ru>
2019-12-20 00:10:51 +0300

    dropMenu

--- app/script.js
+++ app/script.js
@@ -17,7 +17,14 @@ $(document).ready(function () {
 
     $("#phone").inputmask({"mask": "+7 (999) 999-99-99", placeholder: "+7 (___) ___-__-__"});
 
+    $(".js-btn-nav").on("click", function () {
 
+        $(".first__menu").addClass('first__menu-active');
+        $(".first__header").css('flex-direction', 'column');
+        $(".first__logo").css('align-self','flex-start');
+        $(".first__menu-btn-nav",).css('display', 'none');
+        $(".first__menu-link--free").css('display', 'none');
+    })
 
 });
 
